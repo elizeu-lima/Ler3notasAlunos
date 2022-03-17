0c4116432216314e02f96193698321fc2d18b488
elizeu-lima <elizeucoca@gmail.com>
2022-03-16 21:40:17 -0300

    Adicionar arquivos de projeto.

---
+++ Ler3notasAlunos/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+
+            Console.WriteLine("**********************************************");
+
+            Console.WriteLine("");
+
+            Console.Write("Nome do aluno: ");
+
+            
+
+            aluno.Nome = Console.ReadLine();
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Digite as três notas do aluno");
+
+            aluno.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("-----------------------------------------------------------------");
+
+
+            Console.WriteLine("NOTA FINAL = "
+                + aluno.NotaFinal().ToString("F2", CultureInfo.InvariantCulture));
+
+            if (aluno.Aprovado())
+            {
+                Console.WriteLine("APROVADO");
+            }
+            else
+            {
+                Console.WriteLine("REPROVADO");
+
+                Console.WriteLine("*****************************************************");
+
+                Console.WriteLine("FALTARAM "
+                    + aluno.NotaRestante().ToString("F2", CultureInfo.InvariantCulture)+ "PONTOS");
+            }
+
+
+        }
+    }
+}
